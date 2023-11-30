c56406411ca39a28faf12d3def094880ca8938ce
Soham <sohammalakarofficial@gmail.com>
2023-11-30 09:46:20 +0530

    Logo issue deployment

--- client/app/components/Logo.jsx
+++ client/app/components/Logo.jsx
@@ -1,16 +1,12 @@
 "use client";
 import React from "react";
+
 import { useRouter } from "next/navigation";
 const Logo = () => {
   const router = useRouter();
   return (
     <div className="flex" onClick={() => router.push("/")}>
-      <img
-        src="/static/images/Logo.png"
-        alt="Logo"
-        loading="lazy"
-        className="w-12 me-2"
-      />
+      <img src="/logo.png" alt="Logo" loading="lazy" className="w-12 me-2" />
       <p className="text-2xl font-sans font-bold flex items-center me-12">
         AMS
       </p>
