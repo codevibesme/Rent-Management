77553be8eb64dad2878c015e7289205a51c79492
Soham <sohammalakarofficial@gmail.com>
2023-11-30 10:19:34 +0530

    Icon Updated

--- client/app/layout.js
+++ client/app/layout.js
@@ -6,6 +6,20 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "ART Management Services",
   description: "Automated Rent Management & Reports application",
+  icons: {
+    icon: [
+      {
+        media: "(prefers-color-scheme: light)",
+        url: "/logo.png",
+        href: "/logo.png",
+      },
+      {
+        media: "(prefers-color-scheme: dark)",
+        url: "/logo.png",
+        href: "/logo.png",
+      },
+    ],
+  },
 };
 
 export default function RootLayout({ children }) {
